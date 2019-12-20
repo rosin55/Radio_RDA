8092bbdcdee50aadb33e260a0f582eed63507ba8
Rosin <asakhno@gmail.com>
2019-12-20 10:46:21 +0300

    Тест работы с ЕЕПРОМ

--- Radio_AG/Radio_AG.ino
+++ Radio_AG/Radio_AG.ino
@@ -1,9 +1,18 @@
 // Пример использования библиотеки GyverButton, 1- 2- 3- нажатие
 #define BTN_PIN 3   // кнопка подключена сюда (BTN_PIN --- КНОПКА --- GND)
+
+uint16_t EEMEM StartFrequency = 10470;  // начальное значение частоты станции, попадает в файл .eep
+										// если дальше в скрипте есть запись ЕЕПРОМ
+uint16_t LastFrequency = 8570;
+
+#include <avr/eeprom.h> 
 #include "GyverButton.h"
+
 GButton butt1(BTN_PIN);
 void setup() {
 	Serial.begin(9600);
+	Serial.println(LastFrequency);
+	eeprom_write_word((uint16_t *) StartFrequency, LastFrequency); // запись в ЕЕПРОМ последней частоты
 }
 void loop() {
 	butt1.tick();  // обязательная функция отработки. Должна постоянно опрашиваться
@@ -11,4 +20,3 @@ void loop() {
 	if (butt1.isDouble()) Serial.println("Double");     // проверка на двойной клик
 	if (butt1.isTriple()) Serial.println("Triple");     // проверка на тройной клик
 }
-
