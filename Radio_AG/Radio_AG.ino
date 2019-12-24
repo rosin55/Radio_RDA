abd1046303f4fd17c401b6e6065f036030add3ba
Rosin <asakhno@gmail.com>
2019-12-24 15:49:49 +0300

    Опрос кнопок
    Опрос кнопок и установка частоты с отображением в SerialDisplay

--- Radio_AG/Radio_AG.ino
+++ Radio_AG/Radio_AG.ino
@@ -50,27 +50,67 @@ const RADIO_FREQ preset[] PROGMEM = {
 RADIO_INFO StateInfo; // буфер для приема параметров станции
 int    i_sidx = 11; // Стартуем со станции с index=11
 int DlinaSpiska = (sizeof(preset) / sizeof(RADIO_FREQ)) - 1;// длина списка станций
-unsigned long nextFreqTime = 400; // интервал опроса кнопок и ИК пульта
-RADIO_FREQ presetf EEMEM = 10470; // частота последней станции перед выключением
+
 RADIO_FREQ lastf = 0; 
 RADIO_FREQ f = 0;
+unsigned long nextFreqTime = 400; // интервал опроса кнопок и ИК пульта
 uint8_t r;		// новое значение уровня сигнала
 uint8_t lastrssi;  // последнее значение уровня сигнала
 
 RDA5807M radio;    // Создаем класс для  RDA5807 chip radio
 RDSParser rds;     // Класс RDS парсера
 
+void RDS_process(uint16_t block1, uint16_t block2, uint16_t block3, uint16_t block4) {
+  rds.processData(block1, block2, block3, block4);
+} // RDS_process
+
+/// Update the ServiceName text on the display.
+void DisplayServiceName(char *name)
+{
+  Serial.print("RDS:");
+  Serial.println(name);
+ } // DisplayServiceName()
 
 void setup() {
+  // open the Serial port
+    Serial.begin(9600);
+    Serial.println("Radio AG ...");
+    delay(500);
+  // Initialize the Radio 
+    radio.init();
+  // Enable information to the Serial port
+    radio.debugEnable();
+    radio.setBandFrequency(RADIO_BAND_FM, pgm_read_word_near(preset + i_sidx)); // 5. preset.
 	LastFrequency = eeprom_read_word(&StartFrequency); // чтение сохраненной частоты из ЕЕПРОМ
-	Serial.begin(9600);
+    radio.setFrequency(LastFrequency); // запись частоты в радиочип
 	Serial.println(LastFrequency);
+	radio.setMono(false);
+    radio.setMute(false);
+  // radio.debugRegisters();
+    radio.setVolume(5);
+  // setup the information chain for RDS data.
+    radio.attachReceiveRDS(RDS_process);
+    rds.attachServicenNameCallback(DisplayServiceName);
 }
 void loop() {
+	unsigned long now = millis();
 	knUp.tick();  // обязательная функция отработки. Должна постоянно опрашиваться
-	if (knUp.isSingle()) Serial.println("Single");     // проверка на один клик
-	if (knUp.isDouble()) Serial.println("Double");     // проверка на двойной клик
-	if (knUp.isTriple()) Serial.println("Triple");     // проверка на тройной клик
-	eeprom_write_word((uint16_t *) &StartFrequency, LastFrequency); // запись в ЕЕПРОМ последней частоты
-
+	knDown.tick();
+	if (knUp.isSingle()) {
+		Serial.println("Up");   // поиск вверх
+  	    radio.seekUp(true);
+	} 
+	if (knDown.isSingle()) {
+		Serial.println("Douwn");  // поиск вниз
+		radio.seekDown(true);
+	}
+    if (now > nextFreqTime) {
+      f = radio.getFrequency();
+      if (f != lastf) {
+        Serial.println(f);  // вывод новой частоты настройки
+        LastFrequency = f;
+        eeprom_update_word(LastFrequency, StartFrequency);
+      } // if
+    }
+    nextFreqTime = now + 1000;
 }
